c0e0e745c1b83cc2adf2fd787f4b4c8fd4398229
PhuXuth <phuxuth.bloodm@gmail.com>
2014-07-28 00:33:58 +0800

    add jade support

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('static-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+// paths
+var viewHome = path.join(__dirname, 'views');
+var staticHome = path.join(__dirname, 'statics');
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', viewHome);
 app.set('view engine', 'jade');
 
-//app.use(favicon());
+app.use(favicon(staticHome + '/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(require('less-middleware')(staticHome));
+app.use(express.static(staticHome));
 
+/// routes register
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,12 +35,13 @@ app.use(function(req, res, next) {
     next(err);
 });
 
+
 /// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,7 +52,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
